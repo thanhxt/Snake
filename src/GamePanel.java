86b23ae3f60078de0be4bb5d417ddc307bd9050c
thanhxt <thanhtran_xuan@hotmail.de>
2023-03-12 19:39:04 +0100

    checkCollision added with a bug, GameOver added, Keyadapter added

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -9,12 +9,12 @@ public class GamePanel extends JPanel implements ActionListener {
 
     static final int SCREEN_WIDTH = 600;
     static final int SCREEN_HEIGHT = 600;
-    static final int UNIT_SIZE = 25;
+    static final int UNIT_SIZE = 18;
     static final int GAME_UNITS =  (SCREEN_HEIGHT*SCREEN_WIDTH/UNIT_SIZE);
     static final int DELAY = 75; //higher the number, the slower the game
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
-    int bodyParts = 6;
+    int bodyParts = 1;
     int pointsEaten;
     int pointX;
     int pointY;
@@ -46,15 +46,36 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void draw(Graphics g){
         //Draw lines inside our Grid
-        for(int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE;i++){
-            g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
-            g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
-        }
-        //draw Points
-        g.setColor(Color.RED);
-        g.fillOval(pointX,pointY,UNIT_SIZE,UNIT_SIZE);
+        if(running) {
+            for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+                g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+                g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+            }
+            //draw Points
+            g.setColor(Color.RED);
+            g.fillOval(pointX, pointY, UNIT_SIZE, UNIT_SIZE);
 
-       
+            //draw Snake
+            for (int i = 0; i < bodyParts; i++) {
+                if (i == 0) {
+                    System.out.println('H' + i);
+                    System.out.println(running);
+                    g.setColor(Color.YELLOW);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                } else {
+                    System.out.println('B' + i);
+                    g.setColor(Color.ORANGE);
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+                }
+            }
+            g.setColor(Color.RED);
+            g.setFont(new Font("Cambria",Font.BOLD,40));
+            FontMetrics metrics = getFontMetrics(g.getFont());
+            g.drawString("Score: " + pointsEaten,(SCREEN_WIDTH - metrics.stringWidth("Score: " + pointsEaten)) /2, g.getFont().getSize());
+        }
+        else{
+            gameOver(g);
+        }
     }
 
     /**
@@ -84,32 +105,92 @@ public class GamePanel extends JPanel implements ActionListener {
                 break;
             }
         }
+
     }
 
     /**
      * checks how much Points have been eaten
      */
     public void checkPoints(){
-
+        if((x[0] == pointX) && y[0] == pointY){
+            bodyParts++;
+            pointsEaten++;
+            newPoint();
+        }
     }
 
     public void checkCollisions(){
+        //PROBLEM WITH THIS FOR LOOP
+        //GIVES COLLUSION WITH BODY BY MOVING DOWN AND THEN LEFT
+        //if head collides with body
+        for(int i = bodyParts; i > 0 ; i--){
+            if((x[0] == x[i]) && (y[0] == y[i])){
+                running = false;
+            }
+        }
+        //if head touch left border
+        if((x[0] < 0))
+            running = false;
 
+        //if head touch right border
+        if(x[0] > SCREEN_WIDTH)
+            running = false;
+
+        //if head touch top border
+        if(y[0] < 0)
+            running = false;
+
+        //if head touch bottom border
+        if(y[0] > SCREEN_HEIGHT)
+            running = false;
+
+        if(!running)
+            timer.stop();
     }
 
     public void gameOver(Graphics g){
-
+        //GAME OVER Text
+        g.setColor(Color.RED);
+        g.setFont(new Font("Cambria",Font.BOLD,75));
+        FontMetrics metrics = getFontMetrics(g.getFont());
+        g.drawString("GAME OVER",(SCREEN_WIDTH - metrics.stringWidth("GAME OVER")) /2, SCREEN_HEIGHT / 2);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        if(running){
+            move();
+            checkPoints();
+            checkCollisions();
+        }
+        repaint();
     }
 
     public class MyKeyAdpter extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e){
-
+            switch(e.getKeyCode()){
+                case KeyEvent.VK_LEFT:
+                    if(direction != 'R'){
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if(direction != 'L'){
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if(direction != 'D'){
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if(direction != 'U'){
+                        direction = 'D';
+                    }
+                    break;
+            }
         }
     }
 }
